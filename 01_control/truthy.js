7275c9ef73e0a8240e16413c4a6ba7768cce22df
avijit03dev <avijit03dev@gmail.com>
2025-08-19 02:44:14 +0000

    i have work in if else ande switch cash.

---
+++ 01_control/truthy.js
@@ -0,0 +1,12 @@
+const userEmali = []
+if (userEmali) {
+    console.log('Got User E-mail')
+} else {
+    console.log('Dont Have User E-mail')
+}
+
+// false → the Boolean false itself
+// 0 → -0 → 0n → "" → empty string ('' or ``````) -> null ->undefined -> NaN → "Not-a-Number"
+
+// Truthy Values
+// 42,-> -7,-> "hello",-> "0",-> "false",-> " ",-> [],-> [1, 2],-> {},-> { a: 1 },-> function () { },-> () => { },-> Infinity,-> -Infinity,-> 123n,-> Symbol("id")
